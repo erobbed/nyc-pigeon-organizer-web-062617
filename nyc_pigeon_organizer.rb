fa4e5c74791f574279f1af29c4e48dd9b1664385
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-16 17:01:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  #pigeon_names = []
+  #pigeon_attributes = data.keys
+  #colors = data.values[0].keys
+  #genders = data.values[1].keys
+  #locations = data.values[2].keys
+
+  data.each do |attributes, attribute_data|
+    attribute_data.each do |key, value|
+      value.each do |pigeon|
+        if pigeon_list.keys.include?(pigeon)
+          if pigeon_list[pigeon].has_key?(attributes)
+            pigeon_list[pigeon][attributes] << key.to_s
+          else
+            pigeon_list[pigeon][attributes] = [key.to_s]
+          end
+        else
+          pigeon_list[pigeon] = {attributes => [key.to_s]}
+        end
+      end
+    end
+  end
+  #binding.pry
+  pigeon_list
 end
